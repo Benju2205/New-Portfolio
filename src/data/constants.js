4ed9f2992b94e5ebb131b3d4b0a1b82a7e712d83
Benju2205 <120126423+Benju2205@users.noreply.github.com>
2023-11-21 11:32:07 +0530

    updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Benju2205",
   resume:
-    "https://drive.google.com/file/d/1cmUXVINkiyQL53eLZ76MsNEsEnnWC389/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1PcRWx3uTYmBNBh_kugHV2oAFjN3K9jyK/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/benju2205/",
   twitter: "https://twitter.com/benju2205",
   insta: "https://www.instagram.com/benju2205/",
